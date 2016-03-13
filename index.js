47bc8cfe3ebfbcf6c2014b1fd3bf03b5cead88a7
armando <armandomendivil.m@gmail.com>
2016-03-12 21:49:20 -0800

    Add webpack configuration

---
+++ index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class App extends Component {
+
+	render() {
+		return <h3>Hello, World</h3>
+	}
+}
+
+render(
+	<App/>,
+	document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: __dirname,
+        query: {
+          presets: ['react', 'es2015', 'react-hmre']
+        }
+      }
+    ]
+  }
+}
